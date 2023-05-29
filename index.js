90dc01aa1f72d5b8e79884b796aa1e4e7b29d8be
TDTX 1 <tdtx1@TDTXs-MacBook-Pro.local>
2023-05-29 13:11:30 -0700

    added starter code to markdown.js and updated package.json

--- index.js
+++ index.js
@@ -2,3 +2,54 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
 const generateMarkdown = require('./utils/genrateMarkdown');
+
+const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "Please name your Project.",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please describe the purpose of this project.",
+    },
+    {
+      type: "input",
+      name: "link",
+      message: "Please provide a URL where a user can access your deployed application."
+    },
+    {
+      type: "checkbox",
+      name: "license",
+      message: "Please select a license applicable to this project.",
+      choices: ["MIT", "APACHE2.0", "none"],
+    },
+    {
+      type: "input",
+      name: "require",
+      message: "List any project dependencies here.",
+    },
+    {
+      type: "input",
+      name: "features",
+      message: "List key features about this project here.",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message:
+        "State the languages used for this project.",
+    },
+    {
+      type: "input",
+      name: "creator",
+      message: "Write your GitHub username.",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Provide an email address.",
+    },
+    
+]
